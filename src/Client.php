700a067debb7ac50283711e1006de4d88dc1f923
Julien Arcin <julien.arcin@gmail.com>
2019-11-09 12:12:52 +0700

    Upgrade wappalyzer to last version (and update command to launch it)

--- src/Client.php
+++ src/Client.php
@@ -64,7 +64,7 @@ class Client
     public function analyse(string $url)
     {
 
-        $path = realpath(__DIR__.'/../node_modules/wappalyzer/index.js');
+        $path = realpath(__DIR__.'/../node_modules/wappalyzer/cli.js');
         return $this->executeCommandAndReturnResult('node '.$path.' '.$url . '  --max-urls=25 --max-depth=3 --max-wait=3000');
     }
 
